6616f8f905e1de3fa9dd0971c075edc62d625fe4
sala 314 <sala314@poliacademico.edu.loc>
2019-03-14 19:12:39 -0500

    New class

---
+++ Modules/helpers.js
@@ -0,0 +1,5 @@
+export const task = 'study JS'
+export const teams = ['Nacional','Medellin','Envigado']
+export const winner = name => `${name} wins`
+const todos = ['Java', 'C++']
+export default todos
---
+++ Modules/main.js
@@ -0,0 +1,9 @@
+//import { task, teams, winner } from './helpers.js'
+import helpers,{ task, teams, winner } from './helpers.js'
+import {Person} from './Person.js'
+console.log(task)
+console.log(teams)
+console.log(winner(teams[1]))
+console.log(helpers[0])
+const Felipe = new Person('Felipe',24)
+console.log(Felipe.greet())
