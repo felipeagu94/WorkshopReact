a84b4c5c44f8fb60b3d06f36c282ea1a70453eb1
sala 314 <sala314@poliacademico.edu.loc>
2019-03-20 17:51:00 -0500

    Faltantes

--- Modules/main.js
+++ Modules/main.js
@@ -7,3 +7,23 @@ console.log(winner(teams[1]))
 console.log(helpers[0])
 const Felipe = new Person('Felipe',24)
 console.log(Felipe.greet())
+
+(function ConstExplain(){
+    const c_age = 30
+    console.log(c_age)
+    if(true){
+        const c_age = 25
+        console.log(c_age)
+    }
+    console.log(c_age)
+})();
+
+(function ConstExplain(){
+    const person = {nombre: 'Felipe', edad: 24}
+    console.log(c_age)
+    if(true){
+        person.edad = 25
+        console.log(person)
+    }
+    console.log(person)
+})();
