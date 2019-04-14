0ee4f469cdc55979c3573daa2b411c9a69045833
Juan Felipe Agudelo Zapata <felipe-agu@hotmail.com>
2019-04-13 23:02:45 -0500

    Practica 7

---
+++ 7.practica/src/components/main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+	componentDidMount() {
+		const URL = `https://examen-react-workshop.herokuapp.com/examen/`;
+	}
+
+	render() {
+		return (
+			<div className="container">
+			Inicio
+			</div>
+		);
+	}
+}
+
+export default Main;
